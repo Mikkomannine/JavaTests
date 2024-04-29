e101ea22ca884d296a4832ec705a5784e00674ce
Mikkomannine <mikktma@metropolia.fi>
2024-04-29 20:55:26 +0300

    push

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,17 @@
+public class PalindromeChecker {
+    public boolean isPalindrome(String input) {
+
+        String cleanedInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = cleanedInput.length() - 1;
+        while (left < right) {
+            if (cleanedInput.charAt(left) != cleanedInput.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
---
+++ src/shoppingcart/ShoppingCart.java
@@ -0,0 +1,33 @@
+package shoppingcart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+    private Map<String, Double> items;
+
+    public ShoppingCart() {
+        this.items = new HashMap<>();
+    }
+
+    public void addItem(String itemName, double price) {
+        items.put(itemName, price);
+    }
+
+    public void removeItem(String itemName) {
+        items.remove(itemName);
+    }
+
+    public double calculateTotal() {
+        double total = 0.0;
+        for (double price : items.values()) {
+            total += price;
+        }
+        return total;
+    }
+
+    public int getItemCount() {
+        return items.size();
+    }
+}
+
